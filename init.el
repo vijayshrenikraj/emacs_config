0ef32c04eeb9f08edef6c0299ae40539792e21bd
Vijay Shrenikraj <vijayshrenikraj@Vijay.local>
2020-03-23 22:18:38 -0700

    next version

--- init.el
+++ init.el
@@ -29,6 +29,9 @@
 ;; top-bottom-recenter
 (setq recenter-positions '(top bottom middle))
 
+;; also set M-l as recenter-top-bottom
+(global-set-key (kbd "M-l") 'recenter-top-bottom)
+
 ;; set helm-index
 (global-set-key (kbd "M-i") 'helm-imenu)
 
@@ -45,8 +48,8 @@
 ;; line number
 (require 'linum)
 (global-linum-mode t)
-;; use customized linum-format: add a addition space after the line number                                                                      
-(setq linum-format (lambda (line) (propertize (format (let ((w (length (number-to-string (count-lines (point-min) (point-max)))))) (concat "%" 
+;; use customized linum-format: add a addition space after the line number
+(setq linum-format (lambda (line) (propertize (format (let ((w (length (number-to-string (count-lines (point-min) (point-max)))))) (concat "%"
 (number-to-string w) "d  ")) line) 'face 'linum)))
 
 ;; Duplicate-line
@@ -70,8 +73,15 @@
 (setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
 
+;; auto-complete
+(auto-complete-mode t)
+(global-auto-complete-mode t)
+
+;;paranthesis mode
+(show-paren-mode t)
+
 ;;dumb jump
-(dumb-jump-mode)
+;;(dumb-jump-mode)
 
 
 ;; Added by Package.el.  This must come before configurations of
@@ -90,7 +100,7 @@
 (require 'ido)
 (ido-mode t)
 
- 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -101,7 +111,7 @@
     ("624f3b1e86a81d1873b93edc3cce0947f2042bfeebecc480b393ff1e0aa4abfd" default)))
  '(package-selected-packages
    (quote
-    (dumb-jump git-blamed git helm magit haskell-mode ctags-update smart-mode-line diff-hl textmate auto-complete robe rainbow-delimiters rainbow-mode aggressive-indent)))
+    (csv-mode vlf dumb-jump git-blamed git helm magit haskell-mode ctags-update smart-mode-line diff-hl textmate auto-complete robe rainbow-delimiters rainbow-mode aggressive-indent)))
  '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -114,7 +124,7 @@
 ;; requre MELPA
 (require 'package)
 ;; Any add to list for package-archives (to add marmalade or melpa) goes here
-(add-to-list 'package-archives 
+(add-to-list 'package-archives
     '("MELPA" .
       "http://melpa.milkbox.net/packages/"))
 (package-initialize)
@@ -156,3 +166,34 @@
 
 ;; projectile key modifiers
 (setq mac-command-modifier 'super)
+
+
+;;search word under
+(defun xah-search-current-word ()
+  "Call `isearch' on current word or text selection.
+“word” here is A to Z, a to z, and hyphen 「-」 and underline 「_」, independent of syntax table.
+URL `http://ergoemacs.org/emacs/modernization_isearch.html'
+Version 2015-04-09"
+  (interactive)
+  (let ( $p1 $p2 )
+    (if (use-region-p)
+        (progn
+          (setq $p1 (region-beginning))
+          (setq $p2 (region-end)))
+      (save-excursion
+        (skip-chars-backward "-_A-Za-z0-9")
+        (setq $p1 (point))
+        (right-char)
+        (skip-chars-forward "-_A-Za-z0-9")
+        (setq $p2 (point))))
+    (setq mark-active nil)
+    (when (< $p1 (point))
+      (goto-char $p1))
+    (isearch-mode t)
+    (isearch-yank-string (buffer-substring-no-properties $p1 $p2))))
+
+(global-set-key (kbd "M-s") 'xah-search-current-word)
+(global-set-key (kbd "C-s") 'isearch-forward)
+
+;; delete trailing whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
